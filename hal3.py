66b7226da77c20920d8af0c1207984ad0acd5600
Tim Kilåker <tkilaker@gmail.com>
2017-10-29 18:27:29 +0100

    Improved code, improved speech recognition

--- hal3.py
+++ hal3.py
@@ -16,9 +16,19 @@ print ""
 print ""
 print ""
 
-version = "0.4"
+version = "0.5"
 osa = ""
+
 r = sr.Recognizer()
+r.operation_timeout = 3
+
+def listen():
+	print "[Listening...]"
+	with sr.Microphone(device_index=1) as source:
+		r.adjust_for_ambient_noise(source, duration=0.5)
+		speech_input_sound = r.listen(source, timeout=None)
+		speech_input_text = r.recognize_google(speech_input_sound, language="en-US")
+	return speech_input_text
 
 def play(filename):
 	#define stream chunk
@@ -67,7 +77,8 @@ def show_about():
 	print "- 0.2.1: Code Runs Faster (fixed bad code)"
 	print "- 0.2.1.1: RandInt for Variety. Added/edited phrases"
 	print "- 0.3: Removed Mac OS X Dependency. Added More Audio."
-	print "- 0.4: Added Menu\n"
+	print "- 0.4: Added Menu"
+	print "- 0.5: Improved code, improved speech recognition\n"
 	start_menu()
 
 def show_help():
@@ -125,123 +136,93 @@ start_menu()
 print "HAL-9000: Hello, Dave.\n"
 if osa.lower() == "on":
 	runme("""say "Hello Dave." using "Alex" speaking rate 150 modulation 25 pitch 38""")
-print "Listening...\n"
+speech_input_text = listen()
 while True:
 	try:
-		with sr.Microphone() as source:
-			answer = r.listen(source)
-			response = r.recognize_google(answer)
-		print "Analyzing Response...\n"
-
-		if "pod bay" in response or "pod" in response:
-			print ("Dave: " + response)
+		print "[Analyzing Input...]\n"
+		
+		if "pod bay" in speech_input_text.lower() or "pod" in speech_input_text.lower():
+			print ("Dave: " + speech_input_text)
 			print "HAL-9000: I'm sorry Dave. I'm afraid I can't do that."
 			play("audio/cantdo.wav")
-			print "Listening...\n"
-			with sr.Microphone() as source:
-				answer = r.listen(source)
-				response = r.recognize_google(answer)
-
-			print "Analyzing Response..."
-			if "emergency airlock" in response or "air locks" in response:
-				print ("Dave: " + response)
+			speech_input_text = listen()
+			print "[Analyzing Input...]"
+			if "emergency airlock" in speech_input_text.lower() or "air lock" in speech_input_text.lower() or "air locks" in speech_input_text.lower():
+				print ("Dave: " + speech_input_text)
 				print "HAL-9000: Without your space helmet, Dave? You're going to find that rather difficult."
 				play("audio/helmet.wav")
-				print "Listening..."
-				with sr.Microphone() as source:
-					answer = r.listen(source)
-					response = r.recognize_google(answer)
-				print "Analyzing Response..."
-			elif "why" in response or "why not" in response:
-				print ("Dave: " + response)
+				speech_input_text = listen()
+			elif "why" in speech_input_text.lower() or "why not" in speech_input_text.lower():
+				print ("Dave: " + speech_input_text)
 				print "HAL-9000: This mission is too important for me to allow you to jeopardize it."
 				play("audio/mission.wav")
-				print "Listening..."
-				with sr.Microphone() as source:
-					answer = r.listen(source)
-					response = r.recognize_google(answer)
-				print "Analyzing Response..."
+				speech_input_text = listen()
 			else:
-				print ("Dave: " + response)
+				print ("Dave: " + speech_input_text)
 				print "HAL-9000: ..."
 				break
 
-		elif "how are you" in response:
+		elif "how are you" in speech_input_text:
 			coin_flip = random.randint(1,2)
-			print ("Dave: " + response)
+			print ("Dave: " + speech_input_text)
 			if coin_flip == 1:
 				print "HAL-9000: Good evening Dave. Everythings running smoothly. And you?"
 				play("audio/hihal.wav")
 			else:
 				print "HAL-9000: I'm completely operational and all my circuits are functioning perfectly. And you?"
 				play("audio/operational2.wav")
-			print "Listening..."
-			with sr.Microphone() as source:
-				answer = r.listen(source)
-				response = r.recognize_google(answer)
-			print "Analyzing Response..."
-			if "not good" in response or "bad" in response or "not well" in response:
-				print ("Dave: " + response)
+			
+			speech_input_text = listen()
+			print "[Analyzing Input...]"
+			if "not good" in speech_input_text or "bad" in speech_input_text or "not well" in speech_input_text:
+				print ("Dave: " + speech_input_text)
 				print "HAL-9000: Look Dave, I can see you're really upset about this. I honestly think you ought to sit down calmly, take a stress pill, and think things over."
 				play("audio/stresspill2.wav")
-				print "Listening..."
-				with sr.Microphone() as source:
-					answer = r.listen(source)
-					response = r.recognize(answer)
-				print "Analyzing Response..."
-			elif (osa.lower() == 'on') and ('am good' in response or 'fine' in response):
-					print ("Dave: " + response)
- 					print "HAL-9000: I am glad to hear that you are doing well, Dave."
- 					runme("""say "I am glad to hear that you are doing well, Dave." using "Alex" speaking rate 185 modulation 15 pitch 37""")
- 					print "Listening..."
- 					with sr.Microphone() as source:
- 						answer = r.listen(source)
- 						response = r.recognize_google(answer)
- 					print "Analyzing Response..."
+				speech_input_text = listen()
+
+			elif (osa.lower() == 'on') and ('am good' in speech_input_text or 'fine' in speech_input_text):
+				print ("Dave: " + speech_input_text)
+ 				print "HAL-9000: I am glad to hear that you are doing well, Dave."
+ 				runme("""say "I am glad to hear that you are doing well, Dave." using "Alex" speaking rate 185 modulation 15 pitch 37""")
+ 				speech_input_text = listen()
+
  			else:
 				print "Design Flaw!"
-				print "Listening..."
- 				with sr.Microphone() as source:
- 					answer = r.listen(source)
- 					response = r.recognize_google(answer)
- 				print "Analyzing Response..."
+ 				speech_input_text = listen()
 
-		elif "bye" in response or "exit" in response or "by" in response:
-			print ("Dave: " + response)
+		elif "bye" in speech_input_text or "exit" in speech_input_text or "by" in speech_input_text:
+			print ("Dave: " + speech_input_text)
 			break
-		elif "sing" in response: # replace with hals song
-			print ("Dave: " + response)
+		
+		elif "sing" in speech_input_text: # replace with hals song
+			print ("Dave: " + speech_input_text)
 			print "HAL-9000: Daisy, Daisy..."
 			play("audio/daisy.wav")
-			print "Listening..."
-			with sr.Microphone() as source:
-				answer = r.listen(source)
-				response = r.recognize_google(answer)
-			print "Analyzing Response..."
-		elif "about" in response:
-			print ("Dave: " + response)
+			speech_input_text = listen()
+		
+		elif "about" in speech_input_text:
+			print ("Dave: " + speech_input_text)
 			print "HAL-9000 begins to tell you about itself..." #Future transcript
 			play("audio/hal9000.wav")
-			print "Listening..."
-			with sr.Microphone() as source:
-				answer = r.listen(source)
-				response = r.recognize_google(answer)
-			print "Analyzing Response..."
+			speech_input_text = listen()
 
 		else: # if any of the conditions above are not met, except if speech is unintelligable
-			print ("Dave: " + response)
+			print ("Dave: " + speech_input_text)
 			print "HAL-9000: Im sorry Dave. I didn't catch that. What did you say?\a"
-			print "Listening..."
-			with sr.Microphone() as source:
-				answer = r.listen(source)
-				response = r.recognize_google(answer)
-			print "Analyzing Response..."
+			speech_input_text = listen()
+			continue
+	
+	except sr.UnknownValueError:
+		print "Google Speech Recognition could not understand audio"
+		continue
+
+	except sr.RequestError as e:
+		print "Could not request results from Google Speech Recognition service; {0}"
+		continue
+
 	except (LookupError, sr.UnknownValueError) as e:
 		print "Could not understand audio, try again...\a"
-		print "Listening..."
 		continue
 
 print "HAL-9000: This conversation can serve no purpose anymore. Goodbye."
 play("audio/Good_Bye_1_.wav")
-
-
